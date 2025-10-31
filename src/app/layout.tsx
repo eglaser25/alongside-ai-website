7dd089add2e8aee06e94ddceb8837988ea74106b
Evan Glaser <glaser.evan@gmail.com>
2025-10-31 14:02:22 -0600

    adding rb2b script to head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,12 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta httpEquiv="Permissions-Policy" content="payment=*, browsing-topics=(), interest-cohort=()" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html:
+              '!function(key) {if (window.reb2b) return;window.reb2b = {loaded: true};var s = document.createElement("script");s.async = true;s.src = "https://ddwl4m2hdecbv.cloudfront.net/b/" + key + "/" + key + ".js.gz";document.getElementsByTagName("script")[0].parentNode.insertBefore(s, document.getElementsByTagName("script")[0]);}("0OV0VHMZ1R6Z");',
+          }}
+        />
       </head>
       <body className={`${inter.className} antialiased`}>
         <Layout>{children}</Layout>
